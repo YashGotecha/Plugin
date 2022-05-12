4e90ebfd1fabf26fab28afd1709dab35914f9b3e
Yash Gotecha <yash.gotecha@aimdek.com>
2022-05-12 16:07:30 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,27 @@
+import 'react-native-gesture-handler';
+
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+// Importing our screens
+import Home from './src/screens/Home';
+import Chat from './src/screens/Chat';
+
+const Stack = createStackNavigator();
+
+export default function App(){
+    return(
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={Home}/>
+                <Stack.Screen name="Chat" component={Chat}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+/*
+  We are wrapping Home screen and Chat screen in a single stack, 
+  so that we can navigate from Home to Chat screen.
+*/
